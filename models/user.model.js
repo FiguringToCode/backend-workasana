8c667365aa62b5d17992d800e9b85653473743a2
FiguringToCode <marineryash2000@gmail.com>
2025-11-17 11:56:53 +0530

    feat: changed the user schema

--- models/user.model.js
+++ models/user.model.js
@@ -1,7 +1,9 @@
+const { required } = require('joi')
 const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true },
+    password: {type: String, required: true, unique: true},
     email: { type: String, required: true, unique: true }
 })
 
