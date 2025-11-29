e5769c9405c7e4c4b9c244182c1d81df6c43d07a
FiguringToCode <marineryash2000@gmail.com>
2025-11-29 12:07:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -349,7 +349,7 @@ app.post('/tag', verifyJWT, async (req, res) => {
 
 
 
-const PORT = process.env.MONGODB
+const PORT = 5176
 app.listen(PORT, () => {
     console.log("Server connected to port", PORT)
 })
